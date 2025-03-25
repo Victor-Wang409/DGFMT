174825abb3685e866c603ff2566ed108c889ff3f
WangChenHao <skywingsir@gmail.com>
2025-03-25 12:22:35 +0800

    review

--- scripts/t_SNE.sh
+++
@@ -1,6 +0,0 @@
-python t_SNE.py \
---emotion2vec_dir ./emo2vec_large_features \
---hubert_dir ./hubert_large_features \
---model_path ./models/msp_podcast/epoch10/pytorch_model.bin \
---csv_path /home/wangchenhao/Github/Dynamic/csv_files/IEMOCAP.csv \
---output_dir ./results
